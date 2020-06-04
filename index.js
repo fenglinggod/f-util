b8809302baf23d4fe0f85c9f87518b6b0fec7cbd
jiawei.fu <jiawei.fu@openjawtech.com>
2020-06-04 09:43:50 +0800

    outher

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
 import before from './lib/before';
+import camel from './lib/camel';
+import convert from './lib/convert';
+import judge from './lib/judge';
+import injectScript from './lib/injectScript';
+import download from 'download';
+import scroll from './lib/scroll';
+import shuffle from './lib/shuffle';
+import copy from './lib/copy';
+import formArray from './lib/formArray'
+import pattern from './lib/pattern'
+import debouncer from './lib/debouncer'
+import color from './lib/color'
 export {
-  before
+  before,
+  camel,
+  convert,
+  judge,
+  injectScript,
+  scroll,
+  shuffle,
+  copy,
+  formArray,
+  download,
+  pattern,
+  debouncer,
+  color
 }
